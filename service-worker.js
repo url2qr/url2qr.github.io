6f87ab579962c916eb30672d9c08b6e510e65755
Hary Pirajan Mahalingam <hary2309@gmail.com>
2024-05-29 23:38:11 +0200

    Fixes

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@ self.addEventListener("fetch", (event) => {
   }
   const url = new URL(event.request.url);
 
-  if (url.pathname === "/share") {
+  if (url.pathname === "/") {
     const sharedData = {
       link: url.searchParams.get("link"),
     };
